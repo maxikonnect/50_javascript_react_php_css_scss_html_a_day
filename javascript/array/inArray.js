756fa851772db09ef33e72e27e30ce745e27fbc2
maxikonnect <tribillio@gmail.com>
2025-02-15 09:27:12 +0000

    in operator in Arrays

---
+++ javascript/array/inArray.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5];
+
+console.log(1 in arr);
+console.log(2 in arr);
---
+++ javascript/array/sparseArrays.js
@@ -0,0 +1,7 @@
+//CREATING sparse Arrays with new Array
+
+let arr = new Array(5);
+console.log(arr);
+
+arr[8] = 10;
+console.log(arr.length);
