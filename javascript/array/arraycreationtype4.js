61e59d212323aa4ee983fd9596ee422b3f7d39e9
maxikonnect <tribillio@gmail.com>
2025-02-15 08:37:23 +0000

    Creating an array with Array.of

---
+++ javascript/array/arraycreationtype4.js
@@ -0,0 +1,7 @@
+//Array.of() outputs a single digit as Array not length of Array as new Array does
+
+let arr = Array.of(10);
+console.log(arr);
+
+let arr2 = Array.of(10, 20, 30);
+console.log(arr2);
