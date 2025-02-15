cc51c9820539aa99d42b4c46dc3f2401d4cf98d3
maxikonnect <tribillio@gmail.com>
2025-02-15 18:50:45 +0000

    Added toString()

--- javascript/array/array_method_toString.js
+++ javascript/array/array_method_toString.js
@@ -1,3 +1,9 @@
 //toString Method
 let arr = [1, 2, 3, 4];
-console.log(arr.toString()); //=>1, 2, 3, 4
+console.log(arr.toString()); //=>1,2,3,4
+
+let arra = ["a", "b", "c"];
+console.log(arra.toString());//a,b,c
+
+let ar = [1, [2, 4]];
+console.log(ar.toString());//1,2,4
