9fe3a23950ed53c36dcaa0eb0977f9107f73c6d5
maxikonnect <tribillio@gmail.com>
2025-02-15 18:48:05 +0000

    join method

---
+++ javascript/array/arrayTostring_join.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3];
+console.log(arr.join()); //=> 1,2,3
+console.log(arr.join(" "));//=> 1 2 3
+console.log(arr.join("-"));//=>1-2-3
+
+let arra = new Array(10);
+console.log(arra.join("-")); //=>---------
---
+++ javascript/array/array_method_toString.js
@@ -0,0 +1,3 @@
+//toString Method
+let arr = [1, 2, 3, 4];
+console.log(arr.toString()); //=>1, 2, 3, 4
