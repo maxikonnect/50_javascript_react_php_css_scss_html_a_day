dcd673b8282a5a1f92b2af3849a8a64d5817b17d
maxikonnect <tribillio@gmail.com>
2025-02-15 13:46:10 +0000

    find and findIndex method

--- javascript/array/arraymethod_find.js
+++ javascript/array/arraymethod_find.js
@@ -4,4 +4,8 @@ const answer = arr.find(function(x){
     return x % 2 === 0;
 })
 
+const answerFindIndex = arr.findIndex(function(x){
+    return x % 2 === 0;
+})
 console.log(answer);
+console.log(answerFindIndex);
