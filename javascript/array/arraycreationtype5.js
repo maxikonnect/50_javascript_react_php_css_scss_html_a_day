8eaf70ef411d197a97c25ca0cdecdeb09f7f9f66
maxikonnect <tribillio@gmail.com>
2025-02-15 09:04:14 +0000

    reading an array

---
+++ javascript/array/arraycreationtype5.js
@@ -0,0 +1,5 @@
+//Creating an array with Array.from()
+
+let arr = [1, 2, 3, 4, 5];
+let arr2 = Array.from(arr);
+console.log(arr2);
---
+++ javascript/array/readingarray.js
@@ -0,0 +1,14 @@
+//READING AN ARRAY
+
+let a = ["world"];
+a[0] = "Happy";
+console.log(a);
+a[1] = 3.14;
+let i = 2;
+a[i] = 3;
+a[i + 1] = 4;
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3])
