0ada64705001dcc2b6ab1a25320265a13c3ca4ba
maxikonnect <tribillio@gmail.com>
2025-02-15 09:40:13 +0000

    length of an array

---
+++ javascript/array/arraylength.js
@@ -0,0 +1,13 @@
+//EMPTY ARRAY
+let a = [];
+console.log(a);
+
+
+//ARRAY WITH THREE ELEMENTS
+let b = [1, 2, 3];
+console.log(b);
+
+
+let c = [1, 2, 3, 4, 5, 6, 7, 8];
+c.length = 4;
+console.log(c);
