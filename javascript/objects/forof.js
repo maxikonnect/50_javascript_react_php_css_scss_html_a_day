7a25177ffb4496917005acfaffc195b25d4012b3
maxikonnect <tribillio@gmail.com>
2025-02-16 08:18:11 +0000

    the for of

---
+++ javascript/objects/forof.js
@@ -0,0 +1,29 @@
+let x = {a : 1, b: 2};
+let key = "";
+for(let alpha of Object.keys(x)){
+    key += alpha;
+}
+
+console.log(key);//ab
+
+let sum = 0;
+for(let value of Object.values(x)){
+    sum += value;
+}
+console.log(sum)//3
+
+let output = ""
+for(let [key, value] of Object.entries(x)){
+    output += key + " : " + value;
+}
+console.log(output);
+
+let frequency = {};
+for(let letter of "mississippi") {
+    if (frequency[letter]) {
+    frequency[letter]++;
+    } else {
+    frequency[letter] = 1;
+    }
+}
+console.log(frequency) // => {m: 1, i: 4, s: 4, p: 2}
