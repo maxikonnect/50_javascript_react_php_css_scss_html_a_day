15d1a7cce7487c23d1514746ea5b01c206d2c098
maxikonnect <tribillio@gmail.com>
2025-02-16 13:01:38 +0000

    trim

---
+++ javascript/strings/padding.js
@@ -0,0 +1,6 @@
+
+// String padding functions in ES2017
+"x".padStart(3) // => " x": add spaces on the left to a length of 3
+"x".padEnd(3) // => "x ": add spaces on the right to a length of 3
+"x".padStart(3, "*") // => "**x": add stars on the left to a length of 3
+"x".padEnd(3, "-") // => "x--": add dashes on the right to a length of 
