358927178c2e7346c014e1aa8807922c79b740f6
maxikonnect <tribillio@gmail.com>
2025-02-16 16:46:01 +0000

    Arrow Functions

---
+++ javascript/strings/concatrepeat.js
@@ -0,0 +1,5 @@
+// Miscellaneous string methods
+//s.concat("!") // => "Hello, world!": just use + operator instead
+"<>".repeat(5) // => "<><><><><>": concatenate n copies. ES6
+
+console.log(Math.pow(27, 1/3));
---
+++ javascript/strings/trim.js
@@ -0,0 +1,4 @@
+// Space trimming functions. trim() is ES5; others ES2019
+" test ".trim() // => "test": remove spaces at start and end
+" test ".trimStart() // => "test ": remove spaces on left. Also trimLeft
+" test ".trimEnd() // => " test": remove spaces at right. Also trimRight
