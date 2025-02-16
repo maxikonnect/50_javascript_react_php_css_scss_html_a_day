42d16f66e579bef5e7a0299da9a8bd411def8b59
maxikonnect <tribillio@gmail.com>
2025-02-16 12:53:42 +0000

    charAt

---
+++ javascript/strings/charAt.js
@@ -0,0 +1,6 @@
+let s = "Hello, world"; // Start with some text.
+// Inspecting individual (16-bit) characters of a string
+s.charAt(0) // => "H": the first character
+s.charAt(s.length-1) // => "d": the last character
+s.charCodeAt(0) // => 72: 16-bit number at the specified position
+s.codePointAt(0) // => 72: ES6, works for codepoints > 16 bits
