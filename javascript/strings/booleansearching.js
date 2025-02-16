c46d230a27eecf80acd391ffb3b2ccba1e8e391b
maxikonnect <tribillio@gmail.com>
2025-02-16 12:39:56 +0000

    boolean searching

---
+++ javascript/strings/booleansearching.js
@@ -0,0 +1,4 @@
+// Boolean searching functions in ES6 and later
+s.startsWith("Hell") // => true: the string starts with these
+s.endsWith("!") // => false: s does not end with that
+s.includes("or") // => true: s includes substring "or"
