76d49b4e561f67ff2b8be1525ae792e3a8d45e3a
maxikonnect <tribillio@gmail.com>
2025-02-16 11:47:35 +0000

    shorthand

--- javascript/objects/serializingObjects.js
+++ javascript/objects/serializingObjects.js
@@ -2,5 +2,5 @@ let p = {x: 1, y: 2, z: 3};
 let s = JSON.stringify(p);
 let t = JSON.parse(s);
 
-console.log(s);
-console.log(t);
+console.log(s);//{"x":1,"y":2,"z":3}
+console.log(t);//{x: 1, y: 2, z: 3};
---
+++ javascript/objects/shorthandprop.js
@@ -0,0 +1,4 @@
+let x = 1, y = 2;
+let o = {x, y};
+
+console.log(o.x + o.y);
