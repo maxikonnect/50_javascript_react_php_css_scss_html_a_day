f26d819c9766ebaccaaf7bbc045761e1d2d4590b
maxikonnect <tribillio@gmail.com>
2025-02-19 12:20:49 +0000

    match

--- javascript/pattern_matching/regex.js
+++ javascript/pattern_matching/regex.js
@@ -5,6 +5,10 @@ console.log(str.search(/AVA/ui))//1
 
 /* REPLACE */
 
-let str1 = "I love JAVAscript";
-console.log(str1.replace(/javascript/gui, "Javascript"))//I love Javascript
-console.log(str1.replace(/javascript/gui, "Javascript".toUpperCase()))//I love JAVASCRIPT
+let str1 = "I love JAVAscript because jAVascript is good";
+console.log(str1.replace(/javascript/gui, "Javascript"))//I love Javascript because Javascript is good
+console.log(str1.replace(/javascript/gui, "Javascript".toUpperCase()))//I love JAVASCRIPT because JAVASCRIPT is good
+
+
+/* MATCH */
+console.log("7 plus 8 equals 15".match(/\d+/g));//['7', '8', '15']
