e10f4ae8b355d7095f9e597ebcc308a070d6a852
maxikonnect <tribillio@gmail.com>
2025-02-19 14:47:32 +0000

    added new Date()

---
+++ javascript/date_Object/date.js
@@ -0,0 +1,6 @@
+let now = new Date(); // The current time
+console.log(now);
+
+let now1 = new Date(0); // The current time
+console.log(now1);//Thu Jan 01 1970 00:00:00 GMT+0000 (Greenwich Mean Time)
+
