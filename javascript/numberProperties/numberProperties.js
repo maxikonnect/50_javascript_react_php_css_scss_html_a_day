2edbd52653325396681d342b3f046a4bfc5df07d
maxikonnect <tribillio@gmail.com>
2025-02-19 07:41:23 +0000

    learning parseInt

---
+++ javascript/numberProperties/numberProperties.js
@@ -0,0 +1,8 @@
+/*ParseInt Method */
+let x = "20";
+console.log(typeof x);//string
+x = Number.parseInt(x);
+console.log(typeof x)//number
+
+console.log(parseInt("077"));//77
+console.log(parseInt("   123 ")); //123
