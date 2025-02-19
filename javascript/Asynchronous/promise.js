66f48c132899a8ce3b22166ceee2206206e406c6
maxikonnect <tribillio@gmail.com>
2025-02-19 16:49:12 +0000

    promises

---
+++ javascript/Asynchronous/promise.js
@@ -0,0 +1,47 @@
+function orderCoffee(){
+    return new Promise((resolve, reject)=>{
+        let rating = Math.floor(Math.random() * 10);
+
+        console.log(rating);
+
+        if(rating > 5){
+             resolve ("I am feeling great");
+        }
+        else{
+             reject (new Error("We are heading to Starbuck"));
+        }
+    })
+}
+
+orderCoffee()
+    .then(mood => console.log(mood))
+    .catch(err => console.log(err))
+    .finally(()=> console.log("Decision been made"));
+
+
+
+/* PROMISE EXAMPLE 2 */
+const p1 = new Promise((resolve, reject)=>{
+    setTimeout(()=> resolve(console.log(1)), 1000);
+})
+
+const p2 = new Promise((resolve, reject)=>{
+    setTimeout(()=> resolve(console.log(2)), 2000);
+})
+
+const p3 = new Promise((resolve, reject)=>{
+    setTimeout(()=> resolve(console.log(3)), 3000);
+});
+
+const p4 = new Promise((resolve, reject)=>{
+    setTimeout(()=> resolve(console.log(4)), 4000);
+});
+
+const p5 = new Promise((resolve, reject)=>{
+    reject(new Error('reject'));
+})
+
+Promise.all([p1, p2, p3, p4])
+    .then(values => console.log(values))
+    .catch(err => console.log(err))
+    .finally(()=> console.log("Promise Done"));
--- javascript/Timers/timers.js
+++ javascript/Timers/timers.js
@@ -1,3 +1,15 @@
+setTimeout(() => { console.log("Ready..."); }, 1000);
+setTimeout(() => { console.log("set..."); }, 2000);
+setTimeout(() => { console.log("go!"); }, 3000);
+
+
+let clock = setInterval(() => {
+    console.clear();
+    console.log(new Date().toLocaleTimeString());
+}, 1000);
+// After 10 seconds: stop the repeating code above.
+setTimeout(() => { clearInterval(clock); }, 10000);
+
 
 let timer = setInterval(() => {
     for(let i = 0; i <= 10; i++){
@@ -7,3 +19,8 @@ let timer = setInterval(() => {
 
 setTimeout(()=> {clearInterval(timer); }, 10000);
    
+function calculate(x, y){return x + y};
+
+const sum = calculate(10, 20);
+
+setTimeout(()=> {console.log(sum)}, 2000);
