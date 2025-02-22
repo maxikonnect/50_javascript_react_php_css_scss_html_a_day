f4525213612eb4184fda836fe0abc31fa860f729
maxikonnect <tribillio@gmail.com>
2025-02-22 19:02:59 +0000

    More react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,42 +1,10 @@
-import reactLogo from './assets/react.svg'
+
 import { createRoot } from "react-dom/client"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 
 
-
-function Header() {
-    return (
-        <header className="header">
-            <img src={reactLogo} className="nav-logo" alt="React logo" />
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-list-item">Pricing</li>
-                    <li className="nav-list-item">About</li>
-                    <li className="nav-list-item">Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function MainContent() {
-    return (
-        <main className="content">
-            <h1>Reason I am excited to learn React</h1>
-            <ol>
-                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                <li>I am more likely to get a job as a front end developer if I know React</li>
-            </ol>
-        </main>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className="footer">
-            <small>© 2024 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
 
 function App() {
     return (
---
+++ my-react-app/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+export default function MainContent() {
+    return (
+        <main className="content">
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+                <li>React is good to learn and understand</li>
+            </ol>
+        </main>
+    )
+}
