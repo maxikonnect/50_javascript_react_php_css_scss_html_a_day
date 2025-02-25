1dbe956cdaa6a046e663357b1bb36b7a9fe14b02
maxikonnect <tribillio@gmail.com>
2025-02-25 07:07:29 +0000

    setting up scss for my cluade chef project

--- my-travel/src/App.js
+++ my-travel/src/App.js
@@ -1,27 +1,9 @@
-import data from "./data"
-import Joke from "./component/Joke"
+import Header from './component/Header'
+
 export default function App(){
-
-  const dataElement = data.map((entry)=>{
-    return (
-      <Joke 
-        key={entry.id}
-
-        //You can use 
-        //{...entry}
-
-        entry = {entry}
-        //OR You can use
-        /*setup={entry.setup}
-        punchline = {entry.punchline} */
-
-      />
-    )
-  })
-  return (
-    <main>
-      {dataElement}
-    </main>
-    
+  return(
+    <>
+      <Header />
+    </>
   )
 }
